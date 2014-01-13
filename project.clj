8bc0dad9e2dc723efdeb28a9bd2e2f3dc41d60d1
Chris Woodford <chris.woodford@gmail.com>
2014-01-12 21:02:46 -0500

    update the project config

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fitcountr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Just tracking what I did today"
+  :url "https://github.com/chriswoodford/fitcountr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
