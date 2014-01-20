dbf1fe81601499337ae2296db83b273f1d110326
Chris Woodford <chris.woodford@gmail.com>
2014-01-20 13:18:16 -0500

    bring hiccup into the mix

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/java.jdbc "0.2.3"]
                  [postgresql "9.1-901.jdbc4"]
                  [ring/ring-jetty-adapter "1.1.6"]
-                 [compojure "1.1.3"]]
+                 [compojure "1.1.3"]
+                 [hiccup "1.0.2"]]
   :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
   :plugins [[speclj "2.9.1"]]
   :test-paths ["spec"]
