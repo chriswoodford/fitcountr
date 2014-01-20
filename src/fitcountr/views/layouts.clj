7f096f90a2d563905eecfcd3074829366ed25271
Chris Woodford <chris.woodford@gmail.com>
2014-01-20 14:35:58 -0500

    clean up the layout file

--- src/fitcountr/views/layouts.clj
+++ src/fitcountr/views/layouts.clj
@@ -7,11 +7,11 @@
     [:head
       [:meta {:charset "utf-8"}]
       [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
-      [:meta {:name "viewport" :content "width=width=device-width, initial-scale=1.0"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
       [:meta {:description ""}]
       [:meta {:author "Chris Woodford @chriswoodford"}]
       (include-css "/css/bootstrap.min.css"
-                  "/css/bootstrap.theme.css")
+                  "/css/bootstrap-theme.min.css")
       (include-js "/js/jquery.min.js"
                   "/js/bootstrap.min.js"
                   "/js/ember.min.js")
