3f75a77a8ae32e969bcc62dea5bd952fca51609b
Chris Woodford <chris.woodford@gmail.com>
2014-01-20 13:10:56 -0500

    add dependencies for getting setup as a web app

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [postgresql "9.1-901.jdbc4"]]
+                 [postgresql "9.1-901.jdbc4"]
+                 [ring/ring-jetty-adapter "1.1.6"]
+                 [compojure "1.1.3"]]
   :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
   :plugins [[speclj "2.9.1"]]
-  :test-paths ["spec"])
+  :test-paths ["spec"]
+  :main fitcountr.core)
