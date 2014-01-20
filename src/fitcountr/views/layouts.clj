b704ee70e6c10a9154337c354b8b79e44cb73fe7
Chris Woodford <chris.woodford@gmail.com>
2014-01-20 14:23:30 -0500

    update meta tags and add bootstrap files

--- src/fitcountr/views/layouts.clj
+++ src/fitcountr/views/layouts.clj
@@ -1,12 +1,17 @@
 (ns fitcountr.views.layouts
   (:use [hiccup.core :only (html)]
-        [hiccup.page :only (html5 include-css)]))
+        [hiccup.page :only (html5 include-css include-js)]))
 
 (defn application [title & body]
   (html5
     [:head
       [:meta {:charset "utf-8"}]
-      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
+      [:meta {:name "viewport" :content "width=width=device-width, initial-scale=1.0"}]
+      [:meta {:description ""}]
+      [:meta {:author "Chris Woodford @chriswoodford"}]
+      (include-css "/css/bootstrap.min.css"
+                  "/css/bootstrap.theme.css")
+      (include-js "/js/bootstrap.min.js")
       [:title title]]
     [:body body]))
