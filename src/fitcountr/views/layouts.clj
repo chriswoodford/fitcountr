852963aa7e6f63c8c995333cb24072bfc6b8e91c
Chris Woodford <chris.woodford@gmail.com>
2014-01-23 19:34:10 -0500

    add the bootstrap header

--- src/fitcountr/views/layouts.clj
+++ src/fitcountr/views/layouts.clj
@@ -28,8 +28,32 @@
       [:title title]]
     [:body body]))
 
+(defn header []
+  (html
+    [:div {:class "navbar navbar-inverse navbar-fixed-top" :role "navigation"}
+      [:div {:class "container"}
+        [:div {:class "navbar-header"}
+          [:button
+            {:type "button" :class "navbar-toggle" :data-toggle "collapse"
+             :data-target ".navbar-collapse"}
+            [:span {:class "sr-only"} "Toggle navigation"]
+            [:span {:class "icon-bar"}]
+            [:span {:class "icon-bar"}]
+            [:span {:class "icon-bar"}]]
+          [:a {:class "navbar-brand" :href "#"} "Project Name"]]
+        [:div {:class "navbar-collapse collapse"}
+          [:form {:class "navbar-form navbar-right" :role "form"}
+            [:div {:class "form-group"}
+              [:input
+                {:type "text" :class "form-control" :placeholder "Email"}]]
+            [:div {:class "form-group"}
+              [:input
+                {:type "password" :class "form-control"
+                 :placeholder "Password"}]]
+            [:button {:type "submit" :class "btn btn-success"} "Sign in"]]]]]))
+
 (defn application [title template]
   (layout
     title
     template
-    (hbs "{{outlet}}" nil)))
+    (hbs (str (header) "{{outlet}}") nil)))
